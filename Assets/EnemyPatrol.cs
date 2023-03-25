7548ae18d402cc3043dee810ee35d42b69af8c78
Rpzstone <serroenlivy04@gmail.com>
2023-03-25 19:59:32 +0100

    ennemie mais le 2 waypoint marche pas

---
+++ Assets/EnemyPatrol.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    private Transform target;
+    private int destPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position = transform.position;
+        transform.Translate(speed * Time.deltaTime * dir.normalized, Space.World);
+
+
+        // si l ennemi est presque arrive a destination
+        if(Vector3.Distance(transform.position, target.position) < 1f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            Debug.Log ("marche");
+        }
+    }
+}
